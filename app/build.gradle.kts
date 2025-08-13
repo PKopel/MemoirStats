611853c10fe904f6a2d69d6add6229bf17b59735
Paweł Kopel <pawel.kopel2@gmail.com>
2025-08-13 23:21:34 +0200

    fix for krdb and kotlin updates (#110)
    * Bump io.github.xilinjia.krdb:gradle-plugin from 3.2.7 to 3.2.8
    
    Bumps [io.github.xilinjia.krdb:gradle-plugin](https://github.com/xilinjia/krdb) from 3.2.7 to 3.2.8.
    - [Release notes](https://github.com/xilinjia/krdb/releases)
    - [Changelog](https://github.com/XilinJia/krdb/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/xilinjia/krdb/compare/3.2.7...3.2.8)
    
    ---
    updated-dependencies:
    - dependency-name: io.github.xilinjia.krdb:gradle-plugin
      dependency-version: 3.2.8
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.jetbrains.kotlin.android from 2.1.21 to 2.2.0
    
    Bumps [org.jetbrains.kotlin.android](https://github.com/JetBrains/kotlin) from 2.1.21 to 2.2.0.
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.1.21...v2.2.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.jetbrains.kotlin.android
      dependency-version: 2.2.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * update kotlin compiler options
    
    * update build.gradle.kts
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -6,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.memoirstats"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.memoirstats"
@@ -31,13 +33,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         viewBinding = true
         //noinspection DataBindingWithoutKapt
         dataBinding = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.fromTarget("17")
     }
 }
 
@@ -47,7 +52,7 @@ dependencies {
 
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.12.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
 }
 
 buildscript {
     dependencies {
-        classpath("io.github.xilinjia.krdb:gradle-plugin:3.2.7")
+        classpath("io.github.xilinjia.krdb:gradle-plugin:3.2.8")
     }
 }
