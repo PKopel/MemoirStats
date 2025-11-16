a762dabafff1627331917201951264f5acd46de8
Paweł Kopel <pawel.kopel2@gmail.com>
2025-11-16 16:36:14 +0100

    Dependabot update fix (#128)
    * Bump org.jetbrains.kotlin.android from 2.2.10 to 2.2.21
    
    Bumps [org.jetbrains.kotlin.android](https://github.com/JetBrains/kotlin) from 2.2.10 to 2.2.21.
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.2.10...v2.2.21)
    
    ---
    updated-dependencies:
    - dependency-name: org.jetbrains.kotlin.android
      dependency-version: 2.2.21
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump io.github.xilinjia.krdb:gradle-plugin from 3.2.8 to 3.2.9
    
    Bumps [io.github.xilinjia.krdb:gradle-plugin](https://github.com/xilinjia/krdb) from 3.2.8 to 3.2.9.
    - [Release notes](https://github.com/xilinjia/krdb/releases)
    - [Changelog](https://github.com/XilinJia/krdb/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/xilinjia/krdb/compare/3.2.8...3.2.9)
    
    ---
    updated-dependencies:
    - dependency-name: io.github.xilinjia.krdb:gradle-plugin
      dependency-version: 3.2.9
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * update build.gradle.kts
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("com.android.application") version "8.13.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
 
 buildscript {
     dependencies {
-        classpath("io.github.xilinjia.krdb:gradle-plugin:3.2.8")
+        classpath("io.github.xilinjia.krdb:gradle-plugin:3.2.9")
     }
 }
